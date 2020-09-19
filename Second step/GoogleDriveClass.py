e32f9c431b3c75ba65f6064c9f87dc128551a874
Or Lichter <orlichter@gmail.com>
2020-09-19 13:06:22 +0300

    download and color nuclei

---
+++ Second step/GoogleDriveClass.py
@@ -0,0 +1,163 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.http import MediaIoBaseDownload, MediaIoBaseUpload
+from apiclient import errors
+import io
+from typing import Optional
+
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+TYPE2MIME_DICT = {'txt': 'text/plain',
+                  'folder': 'application/vnd.google-apps.folder'}
+
+
+class GoogleClient:
+    def __init__(self):
+        self.default_page_size = 1000
+        self.service = self.login()
+        self.files = []
+        self.folders = []
+
+    def login(self):
+        """Shows basic usage of the Drive v3 API.
+        Prints the names and ids of the first 10 files the user has access to.
+        """
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+
+        service = build('drive', 'v3', credentials=creds)
+        return service
+
+    def list_files(self, file_num: int=100, query='', verbose: bool =False):
+        # Call the Drive v3 API
+        results = self.service.files().list(pageSize=file_num, q=query, fields="nextPageToken, files(id, name, mimeType)").execute()
+        items = results.get('files', [])
+
+        if not items:
+            print('No files found.')
+        else:
+            if verbose:
+                print('Files:')
+                for item in items:
+                    print(u'Got {0} ({1} - {2})'.format(item['name'], item['id'], item['mimeType']))
+            print(f"Loaded {len(items)} files into `client.files`")
+
+    def get_all_files(self, file_type: str='', folder_name: str=None, substring: Optional[str]=None,
+                      save: bool=True):
+        params = {
+            'pageSize': self.default_page_size,
+            'fields': "nextPageToken, files(id, name)"
+        }
+        if file_type:
+            params['q'] = f"mimeType='{TYPE2MIME_DICT[file_type]}'"
+        if folder_name:
+            folder_id = self.get_folder_id(folder_name)
+            if params.get('q'):
+                params['q'] = params['q']+f" and parents in '{folder_id}'"
+            else:
+                params['q'] = f"parents in '{folder_id}'"
+        if substring:
+            if params.get('q'):
+                params['q'] = params['q'] + f" and fullText contains '{substring}'"
+            else:
+                params['q'] = f"fullText contains '{substring}'"
+        page_token = None
+
+        results = []
+        while True:
+            try:
+                if page_token:
+                    params['pageToken'] = page_token
+                files = self.service.files().list(**params).execute()
+
+                results.extend(files['files'])
+                page_token = files.get('nextPageToken')
+                if not page_token:
+                    break
+            except errors.HttpError as  error:
+                print('An error occurred: %s' % error)
+                break
+        if save:
+            self.files = results
+        return results
+
+    def get_all_folders(self, to_print: bool=False):
+        folders = self.get_all_files(file_type='folder', save=False)
+        if to_print:
+            for folder in folders:
+                print(folder)
+        self.folders=folders
+        return folders
+
+    def get_folder_id(self, folder_name: str) -> Optional[str]:
+        """
+        Finds the folder name by folder id
+        Args:
+            folder_name: The name of the folder
+
+        Returns:
+            a string of the folder id
+        """
+        if not self.folders:
+            self.get_all_folders()
+        for folder_dict in self.folders:
+            if folder_dict.get('name') == folder_name:
+                return folder_dict.get('id')
+        raise ValueError(f"Could not find Folder {folder_name}.")
+
+    def download_file(self, file_id: str):
+        # If read more than once make sure to run `file.seek(0)`
+        # before in order to move head of file descriptor to the begining of the file
+        request = self.service.files().get_media(fileId=file_id)
+        fh = io.BytesIO()
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print("Download %d%%." % int(status.progress() * 100))
+        fh.seek(0)
+        # DONT FORGET TO CLOSE fh!!
+        return fh
+
+    # `Normalized and scaled data` folder id: 1OSOnj9XW5JuwC0V_WSJqidreD-NSD6U0
+    def upload_BytesIO_file(self, file_data: io.BytesIO, file_type: str, file_name: str,
+                    parent_folder: str='1OSOnj9XW5JuwC0V_WSJqidreD-NSD6U0', to_close: bool=True):
+        file_metadata = {
+            'name': file_name,
+            'mimeType': TYPE2MIME_DICT[file_type],
+            'parents': [parent_folder]
+        }
+        media = MediaIoBaseUpload(file_data,
+                                mimetype=TYPE2MIME_DICT[file_type],
+                                resumable=True)
+        file = self.service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+        if to_close:
+            file_data.close()
+        print('File ID: %s' % file.get('id'))
+
+
+if __name__ == '__main__':
+    client = GoogleClient()
+    client.get_all_folders(to_print=True)
+    
---
+++ Second step/ImageChange2NucleiClasses.py
@@ -0,0 +1,108 @@
+from PIL import Image, ImageDraw
+import pandas as pd
+from typing import Union, Optional
+from GoogleDriveClass import GoogleClient
+import os
+
+COLOR_DICT = {
+    0: (0, 191, 255),
+    1: (255, 235, 153),
+    2: (255, 26, 26),
+    3: (77, 184, 255),
+    4: (191, 128, 64),
+    5: (204, 204, 255),
+    6: (204, 163, 0),
+    7: (20, 31, 31),
+    8: (153, 153, 153),
+    9: (255, 204, 255),
+}
+
+
+def draw_circle(x: float, y: float, radius: int, color: int,  draw_object: ImageDraw.Draw) -> None:
+    """
+
+    Args:
+        x: x coordinate of center
+        y: y coordinate of center
+        color: a number from 1 to 10 to represent the color of the circle.
+        radius: the radius
+
+    Returns:
+
+    """
+    draw_object.ellipse((x - radius, y - radius, x + radius, y + radius), fill=COLOR_DICT[color])
+
+def get_concat_h(im1: Image, im2: Image) -> Image:
+    dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
+
+
+def nuclei_by_group(image_path: str, csv_path: str, k: int, circle_radius: int=4,
+                       output_path: Optional[str]=None, show: bool=True):
+    original_image = Image.open(image_path)
+    changed_image = Image.open(image_path)
+    df = pd.read_csv(csv_path)
+    draw = ImageDraw.Draw(changed_image)
+
+    for index, row in df.iterrows():
+        draw_circle(row['Centroid X px'], row['Centroid Y px'], circle_radius, row[f'K={k}'], draw)
+    concatenated_image = get_concat_h(original_image, changed_image)
+    if show:
+        concatenated_image.show()
+    if output_path:
+        concatenated_image.save(output_path)
+
+
+def download_and_color_whole_slide(slide_name: str, k: int,output_folder: Optional[str]=None,
+                                   images_folder: str='Normalized Images', data_folder: str='Kmean Mini Batch'):
+    """
+
+    Args:
+        slide_name: The name of the slide i.e MB16CC002
+        k: the number of clusters
+        output_folder: The folder to output all the files to. If left empty prints all results
+        images_folder: The images folder in Google Drive
+        data_folder: The data folder in Google Drive
+
+    Returns:
+
+    """
+    def file_id_by_name(cluster_data_list: list, file_name: str):
+        """
+        searches cluster_data_list for a file that starts with file_name.
+
+        Args:
+            cluster_data_list: a list of file dictionaries in the format {'id':XXXX, 'name':XXXX}
+            file_name: the name of the file
+
+        Returns:
+            the file's id from cluster data_list
+        """
+        for file_dict in cluster_data_list:
+            if file_name in file_dict.get('name'):
+                return file_dict.get('id')
+        return None
+
+    client = GoogleClient()
+    slides = client.get_all_files(folder_name=images_folder, substring=slide_name)
+    kmeans_data = client.get_all_files(folder_name=data_folder, substring=slide_name)
+    if output_folder and not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    for file_dict in slides:
+        slide_file_id, file_name = file_dict.values()
+        data_file_id = file_id_by_name(kmeans_data, file_name[:-4])
+        slide = client.download_file(slide_file_id)
+        data = client.download_file(data_file_id)
+        if output_folder:
+            output_path = output_folder + '/' + file_name
+            nuclei_by_group(slide, data, 4, output_path=output_path, show=False)
+        else:
+            nuclei_by_group(slide, data, 4)
+
+
+# nuclei_by_group('MB16CC002_2f369183-05a9-401b-ae3f-44ddf1f68d9e.SCN - Series 2_Region9.png_HS.png',
+#                 'MB16CC002_2f369183-05a9-401b-ae3f-44ddf1f68d9e.SCN - Series 2_Region9.png_HS.png - Scaled.csv',
+#                 6)
+download_and_color_whole_slide('MB16CC002', 4)
